f901b3e8d2cb5b977a767fac919d37028412ff8d
Kyle Salstrom <kylesalstrom@gmail.com>
2022-06-16 16:25:24 -0700

    adds test controller

---
+++ Controllers/TestController.cs
@@ -0,0 +1,16 @@
+using Microsoft.AspNetCore.Mvc;
+using CFBapi.Model;
+
+namespace CFBapi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TestController : ControllerBase
+{
+
+    [HttpGet()]
+    public Test Get()
+    {
+        return new Test("Hey Now!");
+    }
+}
---
+++ Test.cs
@@ -0,0 +1,11 @@
+namespace CFBapi.Model
+{
+    public class Test
+    {
+        public Test(string response = "")
+        {
+            Response = response;
+        }
+        public string? Response { get; set; }
+    }
+}
