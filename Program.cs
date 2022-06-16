a25ab91f3a1b3fc2fe6f33a949e85604b4638b87
Kyle Salstrom <kylesalstrom@gmail.com>
2022-06-16 12:54:53 -0700

    adds cors policy for dev

--- Program.cs
+++ Program.cs
@@ -2,8 +2,11 @@ using CFBSharp.Client;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+//Enable Cors for use in Dev
+string AllowAnyOrigin = "AllowAnyOrigin";
+builder.Services.AddCors(options => options.AddPolicy(AllowAnyOrigin, policy => policy.AllowAnyOrigin()));
 
+// Add services to the container.
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -20,6 +23,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseCors(AllowAnyOrigin);
 }
 
 app.UseHttpsRedirection();
