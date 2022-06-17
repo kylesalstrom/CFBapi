2c91fa1ae5b8eebc6beba30b22290865c782dad8
Kyle Salstrom <kylesalstrom@gmail.com>
2022-06-16 17:38:27 -0700

    worth a shot

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using Azure.Core;
 using CFBSharp.Client;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,19 +16,39 @@ builder.Services.AddMemoryCache();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Connect to CollegeFootballData.com API
-Configuration.Default.ApiKey.Add("Authorization", builder.Configuration["CFBD_API_KEY"]);
-Configuration.Default.ApiKeyPrefix.Add("Authorization", "Bearer");
-
 var app = builder.Build();
 
+string cfbdAPIkey;
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
     app.UseCors(AllowAnyOrigin);
+    cfbdAPIkey = builder.Configuration["CFBD_API_KEY"];
 }
+else
+{
+    SecretClientOptions options = new SecretClientOptions()
+    {
+        Retry =
+        {
+            Delay= TimeSpan.FromSeconds(2),
+            MaxDelay = TimeSpan.FromSeconds(16),
+            MaxRetries = 5,
+            Mode = RetryMode.Exponential
+         }
+    };
+    var client = new SecretClient(new Uri("https://cfbvault.vault.azure.net/"), new DefaultAzureCredential(),options);
+    KeyVaultSecret secret = client.GetSecret("CFBD-API-KEY");
+    cfbdAPIkey = secret.Value;
+    app.MapGet("/", () => cfbdAPIkey);
+}
+
+
+// Connect to CollegeFootballData.com API
+Configuration.Default.ApiKey.Add("Authorization", cfbdAPIkey);
+Configuration.Default.ApiKeyPrefix.Add("Authorization", "Bearer");
 
 app.UseHttpsRedirection();
 
